76e089468b4eec58f7204c6f9f5820eb522d1c90
Abdulqudus <abdulqudusabubakre@gmail.com>
2021-11-09 21:21:15 +0100

    Add logic to display icons

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const list = document.querySelector('.icon-list');
+
+let start = 0;
+let end = 100;
+
+const currentIcon = iconsaxIcons.slice(start, end);
+
+const fragment = document.createDocumentFragment();
+
+currentIcon.forEach((icon) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-item');
+
+  const listItemIcon = document.createElement('i');
+  listItemIcon.classList.add('list-item__icon', 'isax', `isax-${icon}`);
+
+  listItem.appendChild(listItemIcon);
+
+  fragment.appendChild(listItem);
+});
+
+list.appendChild(fragment);
