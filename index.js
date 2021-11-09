a93c0bed71c3eb0db9e8fb5481960cc8a813baf5
Abdulqudus <abdulqudusabubakre@gmail.com>
2021-11-09 22:19:40 +0100

    Add show more on scroll

--- index.js
+++ index.js
@@ -2,26 +2,60 @@ const list = document.querySelector('.icon-list');
 
 let start = 0;
 let end = 100;
+let perPage = 100;
 
-const currentIcon = iconsaxIcons.slice(start, end);
+const loadIcons = () => {
+  const currentIcons = iconsaxIcons.slice(start, end);
+  const fragment = document.createDocumentFragment();
 
-const fragment = document.createDocumentFragment();
+  currentIcons.forEach((icon) => {
+    const listItemContainer = document.createElement('li');
+    listItemContainer.classList.add('col-6', 'col-md-3', 'col-lg-2');
 
-currentIcon.forEach((icon) => {
-  const listItemContainer = document.createElement('li');
-  listItemContainer.classList.add('col-6', 'col-md-3', 'col-lg-2');
+    const listItem = document.createElement('div');
+    listItem.classList.add('list-item', 'my-3');
 
-  const listItem = document.createElement('div');
-  listItem.classList.add('list-item', 'my-3');
+    const listItemIcon = document.createElement('i');
+    const iconClass = icon.split(' ')[0];
+    listItemIcon.classList.add('list-item__icon', 'isax', `isax-${iconClass}`);
 
-  const listItemIcon = document.createElement('i');
-  listItemIcon.classList.add('list-item__icon', 'isax', `isax-${icon}`);
+    listItem.appendChild(listItemIcon);
 
-  listItem.appendChild(listItemIcon);
+    listItemContainer.appendChild(listItem);
 
-  listItemContainer.appendChild(listItem);
+    fragment.appendChild(listItemContainer);
+  });
 
-  fragment.appendChild(listItemContainer);
+  list.appendChild(fragment);
+};
+
+let timeout;
+const debounce = (fn, params) => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() => {
+    fn(params);
+  }, 500);
+};
+
+const loadMoreDocuments = () => {};
+
+const handleScroll = (event) => {
+  const documentHeight = list.clientHeight;
+  console.log(window.scrollY + window.innerHeight, documentHeight);
+  if (window.scrollY + window.innerHeight >= documentHeight - 200) {
+    console.log('At end');
+    start += perPage;
+    end += perPage;
+    loadIcons();
+  }
+};
+
+// Show more icons when you get to the bottom of the page
+window.addEventListener('scroll', (e) => {
+  debounce(handleScroll, e);
 });
 
-list.appendChild(fragment);
+// Initially load the icons with first 100 icons
+loadIcons();
